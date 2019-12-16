4d64bd78c6b8b35cf614089dd74f2742c38d0982
noor-gate <noorsawhney@protonmail.com>
2019-12-16 21:23:32 +0000

    finished part 2

--- day 2/src/Main.java
+++ day 2/src/Main.java
@@ -10,6 +10,8 @@ public class Main {
         String[] codes = opcode.split(",");
         int[] ops = new int[codes.length];
 
+        // PART 1
+
         // Make opcodes into an array of ints
         for (int i = 0; i < codes.length; i++) {
             ops[i] = Integer.parseInt(codes[i]);
@@ -19,7 +21,7 @@ public class Main {
         // Set required values
         ops[1] = 12;
         ops[2] = 2;
-        
+
         for (int i = 0; i < ops.length; i += 4) {
             if (ops[i] == 1) {
                 ops[ops[i + 3]] = ops[ops[i + 1]] + ops[ops[i + 2]];
@@ -31,5 +33,38 @@ public class Main {
         }
 
         System.out.println(ops[0]);
+
+        // PART 2
+
+        for (int j = 0; j < 100; j++) {
+            for (int k = 0; k < 100; k++) {
+
+                // Reset memory
+                for (int i = 0; i < codes.length; i++) {
+                    ops[i] = Integer.parseInt(codes[i]);
+                }
+
+                // Set values
+                ops[1] = j;
+                ops[2] = k;
+
+
+                for (int i = 0; i < ops.length; i += 4) {
+                    if (ops[i] == 1) {
+                        ops[ops[i + 3]] = ops[ops[i + 1]] + ops[ops[i + 2]];
+                    } else if (ops[i] == 2) {
+                        ops[ops[i + 3]] = ops[ops[i + 1]] * ops[ops[i + 2]];
+                    } else {
+                        break;
+                    }
+                }
+
+                if (ops[0] == 19690720) {
+                    System.out.println(100 * j + k);
+                }
+
+            }
+        }
+
     }
 }
